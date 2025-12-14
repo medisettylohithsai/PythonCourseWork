01242273af382721723e5155f1cccd6a45d0974f
unknown <lohithmedisetty@gmail.com>
2025-12-14 16:00:03 +0530

    DataTypes

---
+++ ClassWork.py/1.DataTypes.py
@@ -0,0 +1,33 @@
+# Python Data Types Programs
+
+# Integer
+num = 10
+print("Value:", num, "| Data Type:", type(num))
+
+# Float
+pi = 3.14
+print("Value:", pi, "| Data Type:", type(pi))
+
+# String
+name = "Lohith"
+print("Value:", name, "| Data Type:", type(name))
+
+# Boolean
+is_student = True
+print("Value:", is_student, "| Data Type:", type(is_student))
+
+# List
+my_list = [1, 2, 3, 4]
+print("Value:", my_list, "| Data Type:", type(my_list))
+
+# Tuple
+my_tuple = (5, 6, 7)
+print("Value:", my_tuple, "| Data Type:", type(my_tuple))
+
+# Dictionary
+my_dict = {"name": "Lohith", "age": 20}
+print("Value:", my_dict, "| Data Type:", type(my_dict))
+
+# Set
+my_set = {1, 2, 3}
+print("Value:", my_set, "| Data Type:", type(my_set))
